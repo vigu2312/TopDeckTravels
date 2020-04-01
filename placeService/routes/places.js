80c42a3b7c60d150d37e7a61fd45cb50589fe5bb
Anish Tuli <tulianish@Anishs-MacBook-Pro.local>
2020-04-01 20:55:53 -0300

    Restructured Coordinate routes

--- placeService/routes/places.js
+++ placeService/routes/places.js
@@ -3,12 +3,16 @@ const router  = express.Router();
 const Place = require('../models/Place');
 const Coordinate = require('../models/Coordinate'); 
 
-
-router.get('/' ,async (req,res) => {
+router.get('/coordinate/:placeName', async (req,res) => {
     try{
-        console.log("placeName");
-        const places = await Place.find();
-        res.send(places);
+        console.log("Here");
+        if (req.params.placeName) {
+            const query = { city : req.params.placeName}
+        console.log(query);
+        const coordinate = await Coordinate.find(query);
+        console.log(coordinate);
+        res.send(coordinate);
+    }
     }
     catch(error){
         res.json({message:error});
@@ -16,19 +20,19 @@ router.get('/' ,async (req,res) => {
     }
 });
 
-router.get('/:placeName' ,async (req,res) => {
+router.get('/coordinate/', async (req,res) => {
     try{
-
-        console.log(req.params.placeName);
-        const place = await Place.find({name: req.params.placeName});
-        console.log(place);
-        res.send(place);
-    }
+        const coordinate = await Coordinate.find({});
+        console.log(coordinate);
+        res.send(coordinate);
+        }
     catch(error){
         res.json({message:error});
 
     }
 });
+
+
 router.get('/byId/:Id' ,async (req,res) => {
     try{
 
@@ -43,10 +47,23 @@ router.get('/byId/:Id' ,async (req,res) => {
     }
 });
 
-router.get('/coordinate' ,async (req,res) => {
+router.get('/byName/:placeName' ,async (req,res) => {
+    try{
+        console.log(req.params.placeName);
+        const place = await Place.find({name: req.params.placeName});
+        console.log(place);
+        res.send(place);
+    }
+    catch(error){
+        res.json({message:error});
+
+    }
+});
+
+router.get('/' ,async (req,res) => {
     try{
         console.log("placeName");
-        const places = await Coordinate.find();
+        const places = await Place.find();
         res.send(places);
     }
     catch(error){
@@ -55,20 +72,5 @@ router.get('/coordinate' ,async (req,res) => {
     }
 });
 
-router.get('coordinate/:placeName', async (req,res) => {
-    try{
-
-        console.log(req.params.placeName);
-        const query = { city : req.params.placeName}
-        console.log(query);
-        const coordinate = await Coordinate.find(query);
-        console.log(coordinate);
-        res.send(coordinate);
-    }
-    catch(error){
-        res.json({message:error});
-
-    }
-});
 
 module.exports = router;
